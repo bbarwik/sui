db388e8858a1dceb1caf0fa42a533550a416f6e7
William Smith <williamprincesmith@gmail.com>
2025-01-28 13:35:24 -0800

    Address review comments

--- crates/sui-core/src/authority.rs
+++ crates/sui-core/src/authority.rs
@@ -2905,6 +2905,7 @@ impl AuthorityState {
     }
 
     #[allow(clippy::disallowed_methods)] // allow unbounded_channel()
+    #[allow(clippy::too_many_arguments)]
     pub async fn new(
         name: AuthorityName,
         secret: StableSyncAuthoritySigner,
